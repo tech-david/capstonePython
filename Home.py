4b91a51c7d3b16b45eaba4d9895ff41c8b7e492f
David Mejia <43398304+tech-david@users.noreply.github.com>
2022-08-17 19:18:38 -0700

    updates to reflect work completed
    Updated hoome page and readme file

--- Home.py
+++ Home.py
@@ -16,8 +16,28 @@ st.markdown(
     - You are here. 
     ### About
     - Navigate to 'About'
-      - Provides information about application and its use
+      - Provides information about application and its use.
     ### DataExploration
     - Raw data exploration, consists of interactive dataframes and plots
-    ### Elec
+    ### Processed Data Exploration
+    - Data that has gone through initial phase of cleaning, which includes interpolation and filling NA with 0 where appropriate.
+    ### Reports
+    - Reports generated from datasets that include statistical metrics such as quartiles, mean, median, standard deviation
+    and more.
+    - Natural Gas
+    - Electricity
+    - Oil
+    - Home
+    - Feature
+    - Model Data
+    ### Feature Exploration
+    - Features that have been extracted through monthly price changes of each variable.
+    ### Standardized Data
+    - Price changes after standardization.
+    ### Model Data
+    - Data ready for the model, exploration includes correlation maps and variance inflation factor metric.
+    ### Train Test Split
+    - Data after being split into training and testing data sets.  Testing data was split from the most recent 5 year window, representative of a typical business cyccle.
+    ### Model Results
+    - Statistical results of the model tested, current reports include accuracy, confusion matrix, and ROC curve.  More reports to come including R2, and p-value metrics.
     """)
--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - streamlit-pandas-profiling 0.1.3
 - sklearn 1.1.2
 - tkinter 8.6.12 (conda install)
-- statsmodel 0.13.2
+- statsmodels 0.13.2
 ## Instructions
 > Once cloned, enter the following command on Powershell/Command line (depending on OS) to run locally 
 - `streamlit run Home.py`
